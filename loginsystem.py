7a20fac0960b1dc6b15985dac1002cb4698212aa
id3at <58143208+id3at@users.noreply.github.com>
2020-01-28 14:46:26 +0100

    Add files via upload

---
+++ loginsystem.py
@@ -0,0 +1,24 @@
+"""
+System logowania, na bazie obiektów słownika.
+Z bazą danych logsys.txt.
+
+"""
+
+import ast
+import getpass
+
+nazwa = {input("Podaj login: "): getpass.getpass("Podaj hasło: ")}
+
+with open("logsys.txt", "r") as t:
+    p = t.readlines()
+
+has = []
+for t in p:
+    c = ast.literal_eval(t)
+    if nazwa == c:
+        has.append(c)
+
+if len(has) == 1:
+    print("Prawidłowe logowanie")
+else:
+    print("Nieprawidlowe logowanie. Złe hasło lub login")
